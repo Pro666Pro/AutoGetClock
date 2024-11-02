8c779af586c4883a5f71cee64e5f001934d8d0a0
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-11-03 02:22:10 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -36,13 +36,16 @@ game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(26.34327
 end	           
 elseif game.PlaceId == 102908326578005 then
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Script By Nexer1234" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "Yay"})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Step 1 Completed" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "Yay"})
 for i,v in ipairs(game:GetService("Workspace"):GetDescendants()) do
             if v.ClassName == "ClickDetector" then
                 fireclickdetector(v)
             end
         end
 wait(1)
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Step 2 Completed" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "Yay"})
 fireclickdetector(workspace.Furniture.jorgisDresser.Drawer.Handle.ClickDetector)
 wait(1)
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Step 3 Completed" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "Yay"})
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(26.34327507019043, 4.055271148681641, -1.8455318212509155)
 end
